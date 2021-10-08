2de3453bcf80c8f6e7f7753c73111151ccaf9cc2
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2021-10-08 14:56:33 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Julie Mwol working!')
+            chalk.green.bold('✅ Nandhutty working!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
